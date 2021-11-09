f4698e8288b9dadc18b85363f5d85f282558499d
m koner <mdoukoner@gmail.com>
2021-11-10 03:17:40 +0530

    url for backend updated

--- src/contants.js
+++ src/contants.js
@@ -1,10 +1,3 @@
 const DEVELOPMENT_URL = "http://localhost:5454";
 const PRODUCTION_URL = "https://my-zomato-like-backend.herokuapp.com";
-  
-
-if (process.env.NODE_ENV==='development') {
-    exports.API_URL = DEVELOPMENT_URL;
-}  else {
-    exports.API_URL = PRODUCTION_URL;
-}
-
+exports.API_URL = PRODUCTION_URL;
