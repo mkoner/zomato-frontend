abbd5faabf4c2a1bb2e89dfc9d6d8df4b1e46f3c
m koner <mdoukoner@gmail.com>
2021-11-10 08:05:26 +0530

    adding a file for google auth verification

--- src/Router.js
+++ src/Router.js
@@ -1,24 +1,22 @@
 import React, { Component } from 'react'
-import { Route, BrowserRouter } from 'react-router-dom';
+import { Route, BrowserRouter,Link } from 'react-router-dom';
 
 import Home from './components/Home';
 import Filter from './components/Filter';
 import Details from './components/Details';
 import Header from './components/Header';
 
-const GoogleVerif= './google06d15073cc6466c0.html'
 
 
 export default class Router extends Component {
     render() {
         return (
             <BrowserRouter>
-                <Header />
+                <Route path="/google06d15073cc6466c0.html" render={() => <Link to="./google06d15073cc6466c0.html"></Link>} />
                 <Route exact path="/" component={Home} />
                 <Route path="/home" component={Home} />
                 <Route path="/filter" component={Filter} />
                 <Route path="/details" component={Details} />
-                <Route path="/google06d15073cc6466c0.html" render={() => <div>{GoogleVerif}</div>} />
             </BrowserRouter>
         )
     }
